18a9525d1c151407cdcb64e9c2f9e1bdae5750d3
Baptiste ENTAT <baptiste.entat@epita.fr>
2022-10-26 17:39:32 +0200

    add division function / unittest

--- unittest/Test_StarterPack/TestCalculator/TestCalculator/UnitTest1.cs
+++ unittest/Test_StarterPack/TestCalculator/TestCalculator/UnitTest1.cs
@@ -1,15 +1,37 @@
+using System;
 using Xunit;
-using Calculator;
 
 namespace TestCalculator
 {
-    public class 
-        UnitTestCalculator
+    public class UnitTestCalculator
     {
-        [Fact]
-        public void TestAdd2()
+        
+        //Add2 function
+        [Theory]
+        [InlineData(1, 2, 3)]
+        [InlineData(-4, -6, -10)]
+        [InlineData(-2, 2, 0)]
+        [InlineData(int.MinValue, -1, int.MaxValue)]
+        public void TestAdd2(int a, int b, int expected)
         {
-            Assert.Equal(11, Calculator.Calculator.Add2(5, 6));
+            Assert.Equal(expected, Calculator.Calculator.Add2(a, b));
+        }
+
+        
+        //Div function 
+        [Theory]
+        [InlineData(3, 2, 1)]
+        [InlineData(0, 5, 0)]
+        [InlineData(4, 4, 1)]
+        public void TestDiv(int a, int b, int expected)
+        {
+            Assert.Equal(expected, Calculator.Calculator.Div(a, b));
+        }
+
+        [Fact]
+        public void TestThrowDivDivByZero()
+        {
+            Assert.Throws<DivideByZeroException>((() => Calculator.Calculator.Div(1, 0)));
         }
     }
 }
